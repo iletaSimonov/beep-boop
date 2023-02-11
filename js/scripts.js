476bebc699e9f3db72f4ad79e9305c14d0b83962
Ileta Simonov <ileta.simonov@yahoo.com>
2023-02-11 14:46:25 -0800

    adds a method to turn a 3 into a string 'Wont you be my neighbor?'

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,13 @@
 function beepBoop(number) {
   let result = [];
   for (let i = 0; i <= number; i++) {
+    if (i.toString().includes("30"){
+      result.push("Wont you be my neighbor?");
+    })
+    
     result.push(i);
   }
   return result;
+
+  
 }
